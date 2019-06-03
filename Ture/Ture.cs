6ded1df9b8573f6d03e08defdd3352b1f8c954cf
alenvall <erik.alenvall@gmail.com>
2019-06-03 20:32:49 +0200

    Added project files and setup simple logging

---
+++ Ture/Ture.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ture
+{
+    public class Ture
+    {
+        static void Main(string[] args)
+        {
+            LogManager.LoadConfiguration("Nlog.config");
+            var log = LogManager.GetCurrentClassLogger();
+            log.Debug("Woof!");
+
+            Console.ReadLine();
+        }
+    }
+}
