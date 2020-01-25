295a6798633e3547a8b66c0da75c98e43344d811
alenvall <erik.alenvall@gmail.com>
2020-01-25 21:53:32 +0100

    Changed from .NET to .NET Core 3.1

---
+++ TureNET/Ture/Ture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ture
+{
+    class Ture
+    {
+        static void Main(string[] args)
+        {
+            Log("Woof!");
+        }
+
+        static void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
